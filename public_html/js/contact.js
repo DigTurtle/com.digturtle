c3aeb0afe19844cdb6b1e27afdd346140faa3add
justisin7 <jdizon001@regis.edu>
2015-07-26 21:46:08 -0600

    Create all html css and js: about, apps, websites and contact ***** index is home page

--- public_html/js/contact.js
+++ public_html/js/contact.js
@@ -1,21 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
-  ie 9+
-  using 1.3
-*/
-var myApp = angular.module("myApp",[]);
-
-myApp.controller("formCtrl",["$scope", function($scope){
-  
-  $scope.submitForm = function(){
-    if($scope.form.$valid){
-      alert("Form Submitted!");
-    }
-  };
-  
-}]);
+var app = angular.module('myApp', []);
+app.controller('validateCtrl', function ($scope) {
+    $scope.user = '';
+    $scope.email = '';
+});
