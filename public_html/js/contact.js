1b7dc0a58d2fe77263516767d8664813069ab6c8
Brandon Ward <ward486@regis.edu>
2015-07-26 21:03:24 -0600

    Added contact form, not working quite right yet

---
+++ public_html/js/contact.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+  ie 9+
+  using 1.3
+*/
+var myApp = angular.module("myApp",[]);
+
+myApp.controller("formCtrl",["$scope", function($scope){
+  
+  $scope.submitForm = function(){
+    if($scope.form.$valid){
+      alert("Form Submitted!");
+    }
+  }
+  
+}]);
